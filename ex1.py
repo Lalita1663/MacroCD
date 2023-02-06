b36b1bd0ba3125fe367cb0b63cb234e4a43fdde2
Lalita1663 <99269292+Lalita1663@users.noreply.github.com>
2023-02-06 19:50:48 +0530

    Before execution

--- ex1.py
+++ ex1.py
@@ -1,25 +1,27 @@
 '''
- Lalita Purohit
+Lalita Purohit
 Nidhi Jangir
 
 SIngle line Macro  ( More then one Macro can be defined )
+SIngle line NESTED MACRO
+
 '''
 
 
 
+$def &&  and @end
+$def ARANGE (a>25 && a<50) @end
 
-
-
-f=1
-x=4
-y=90
-
-if((f<5) AND (x<=20 OR y<=45)):
-
-        print("Your PC will always work fine...\n")
+a=30
+        
+if(ARANGE):
+       print("Within range\n")
 else:
-     print("Infront of the maintenance man\n")      
-      
+      print("Out of range\n")        
+        
+
+
+
 
 
 
